7b10c51b9245ac2f6b554b00c18fabcfaa9f50f5
Emil <emilnovikov392@gmail.com>
2024-04-16 18:50:49 +0600

    [Feat] user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserDto } from './user.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getUsers() {
+    return this.userService.getAll();
+  }
+
+  @Get(':id')
+  async getOneUser(@Param('id') id: string) {
+    return this.userService.getById(id);
+  }
+
+  @Post()
+  async createUser(@Body() dto: UserDto) {
+    return this.userService.create(dto);
+  }
+
+  @Patch(':id')
+  async changeEmail(@Param('id') id: string, @Body() dto: UserDto) {
+    return this.userService.change(id, dto);
+  } 
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    return this.userService.delete(id);
+  }
 }
